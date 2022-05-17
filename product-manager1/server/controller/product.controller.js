ba6a55a4b173175614e0c5b355e8dfe7a21fe498
Tculls <tanner.culley@gmail.com>
2022-05-17 13:24:47 -0700

    Full Stack mern

--- product-manager1/server/controller/product.controller.js
+++ product-manager1/server/controller/product.controller.js
@@ -16,3 +16,27 @@ module.exports.addProduct = (req, res) => {
         .then((newProduct) => res.json(newProduct))
         .catch((err)=> console.log(err));
 };
+
+module.exports.oneProduct = (req, res) => {
+    console.log(req)
+    Product.findOne({_id: req.params.id})
+    .then(product=>res.json(product))
+    .catch(err=>res.json(err))
+
+}
+
+module.exports.updateProduct = (req, res) => {
+    Product.findOneAndUpdate(
+        {_id: req.params.id},
+        req.body,
+        {new:true, runValidators:true}
+        )
+        .then(product=>res.json(product))
+        .catch(err=>res.json(err))
+}
+
+module.exports.deleteProduct = (req,res) => {
+    Product.deleteOne({_id: req.params.id})
+        .then(result => res.json(result))
+        .catch(err => res.json(err))
+}
